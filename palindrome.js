20527337427e729290ebda1cf8e749cbf1c99aeb
Tray Harrington <traylon.harrington@gmail.com>
2025-03-11 14:40:01 -0500

    Complete functional programming exercises

--- palindrome.js
+++ palindrome.js
@@ -1,10 +1,38 @@
-// Reverses a string.
-function reverse(string) {
-	return Array.from(string).reverse().join("");
+// Adds `reverse` to all strings.
+String.prototype.reverse = function reverse() {
+	return Array.from(this).reverse().join("");
 }
 
-// Returns true for a palindrom, false otherwise.
-function palindrome(string) {
-	let processedContent = string.toLowerCase();
-	return processedContent === reverse(processedContent);
+// Defines a Phrase object.
+function Phrase(content) {
+	this.content = content;
+
+	this.processor = function processor(string) {
+		return string.toLowerCase();
+	}
+
+	// Returns content processed for palindrome testing.
+	this.processedContent = function processedContent() {
+		return processor(this.content);
+	}
+
+	// Returns true for a palindrom, false otherwise.
+	this.palindrome = function palindrome() {
+		return this.processedContent() === this.processedContent().reverse();
+	}
 }
+
+/*
+// Defines a TranslatedPhrase object.
+function TranslatedPhrase(content, translation) {
+	this.content = content;
+	this.translation = translation;
+
+	// Returns content processed for palindrome testing.
+	this.processedContent = function processedContent() {
+		return processor(this.translation);
+	}
+}
+
+TranslatedPhrase.prototype = new Phrase();
+*/
