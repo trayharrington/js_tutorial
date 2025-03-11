a543b99b19ab074a2bf6fc0509647a187673decb
Tray Harrington <traylon.harrington@gmail.com>
2025-03-11 10:43:14 -0500

    Add more functional programming practice with filter and reduce

--- functional.js
+++ functional.js
@@ -22,3 +22,58 @@ function functionalUrls(elements) {
 	return elements.map(element => urlify(element));
 }
 console.log(functionalUrls(states));
+
+// singles: Imperative version
+function imperativeSingles(elements) {
+	let singles = [];
+	elements.forEach(function(element){
+		if (element.split(/\s+/).length === 1) {
+			singles.push(element);
+		}
+	});
+	return singles;
+}
+console.log(imperativeSingles(states));
+
+// singles: Functional version
+function functionalSingles(elements) {
+	return elements.filter(element => element.split(/\s+/).length === 1);
+}
+console.log(functionalSingles(states));
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// sum: Imperative solution
+function imperativeSum(elements) {
+	let total = 0;
+	elements.forEach(function(n) {
+		total += n;
+	});
+	return total;
+}
+console.log(imperativeSum(numbers));
+
+// sum: Functional solution
+function functionalSum(elements) {
+	return numbers.reduce((total, n) => {return total += n;});
+}
+console.log(functionalSum(numbers));
+
+// lengths: Imperative solution
+function imperativeLengths(elements) {
+	let lengths = {};
+	elements.forEach(function(element) {
+		lengths[element] = element.length;
+	});
+	return lengths;
+}
+console.log(imperativeLengths(states));
+
+// lengths: Functional solution
+function functionalLengths(elements) {
+	return elements.reduce((lengths, element) => {
+										lengths[element] = element.length;
+										return lengths;
+									}, {});
+}
+console.log(functionalLengths(states));
